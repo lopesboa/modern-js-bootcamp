3d9a3d2ed7b2eb92e21f0093a92a625bbf2dc09a
lopesboa <lopesboa1@gmail.com>
2019-12-31 15:47:38 -0300

    Refactoring our method with shorthand syntax

--- 12-object-methods-n-this/methods-to-objects.js
+++ 12-object-methods-n-this/methods-to-objects.js
@@ -1,3 +1,16 @@
+// const math = {
+//   numbers: [1, 2, 3, 4, 5],
+//   add: function(x, y) {
+//     return x + y;
+//   },
+//   multply: function(x, y) {
+//     return x * y;
+//   }
+// };
+
+/**
+ * USING SHORTHAND SYNTAX
+ */
 const math = {
   numbers: [1, 2, 3, 4, 5],
   add(x, y) {
@@ -5,5 +18,15 @@ const math = {
   },
   multply(x, y) {
     return x * y;
+  }
+};
+
+const auth = {
+  username: 'TommyBot',
+  login() {
+    console.log('LOGGED YOU IN');
+  },
+  logout() {
+    console.log('GOODBYE');
   }
 };
